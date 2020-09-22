676fb6ad0001f852f5b1621b468d2100e0167a28
neosarchizo <neosarchizo@neosarcuimacpro.lan>
2020-09-22 21:05:11 +0900

    feat: update qre1113gr

--- neosarchizo/components/drivers/qre1113gr/qre1113gr.c
+++ neosarchizo/components/drivers/qre1113gr/qre1113gr.c
@@ -8,6 +8,7 @@
 #include "nrf_delay.h"
 #include "nrf_log.h"
 #include "n_saadc.h"
+#include "multiplexer.h"
 
 APP_TIMER_DEF(m_qre1113gr_timer);
 
@@ -17,9 +18,10 @@ static volatile bool m_reading = false;
 static volatile uint8_t m_idx_reading = 0;
 static qre1113gr_cb_t m_on_ir_measured = NULL;
 
-static void on_a_measured(uint16_t value) {
+static void on_a_measured(uint16_t value)
+{
     NRF_LOG_INFO("on_a_measured : %d", value);
-    
+
     m_temp_data.a = value < QRE1113GR_DETECT_DISTANCE;
 
     nrf_gpio_pin_write(QRE1113GR_PIN_A_K, 1);
@@ -27,9 +29,10 @@ static void on_a_measured(uint16_t value) {
     app_timer_start(m_qre1113gr_timer, APP_TIMER_TICKS(QRE1113GR_WAIT_INTERVAL_MS), NULL);
 }
 
-static void on_b_measured(uint16_t value) {
+static void on_b_measured(uint16_t value)
+{
     NRF_LOG_INFO("on_b_measured : %d", value);
-    
+
     m_temp_data.b = value < QRE1113GR_DETECT_DISTANCE;
 
     nrf_gpio_pin_write(QRE1113GR_PIN_B_K, 1);
@@ -37,9 +40,10 @@ static void on_b_measured(uint16_t value) {
     app_timer_start(m_qre1113gr_timer, APP_TIMER_TICKS(QRE1113GR_WAIT_INTERVAL_MS), NULL);
 }
 
-static void on_c_measured(uint16_t value) {
+static void on_c_measured(uint16_t value)
+{
     NRF_LOG_INFO("on_c_measured : %d", value);
-    
+
     m_temp_data.c = value < QRE1113GR_DETECT_DISTANCE;
 
     nrf_gpio_pin_write(QRE1113GR_PIN_C_K, 1);
@@ -47,12 +51,58 @@ static void on_c_measured(uint16_t value) {
     app_timer_start(m_qre1113gr_timer, APP_TIMER_TICKS(QRE1113GR_WAIT_INTERVAL_MS), NULL);
 }
 
-static void on_d_measured(uint16_t value) {
+static void on_d_measured(uint16_t value)
+{
     NRF_LOG_INFO("on_d_measured : %d", value);
-    
+
     m_temp_data.d = value < QRE1113GR_DETECT_DISTANCE;
 
     nrf_gpio_pin_write(QRE1113GR_PIN_D_K, 1);
+
+    app_timer_start(m_qre1113gr_timer, APP_TIMER_TICKS(QRE1113GR_WAIT_INTERVAL_MS), NULL);
+}
+
+static void on_e_measured(uint16_t value)
+{
+    NRF_LOG_INFO("on_e_measured : %d", value);
+
+    m_temp_data.e = value < QRE1113GR_DETECT_DISTANCE;
+
+    nrf_gpio_pin_write(QRE1113GR_PIN_E_K, 1);
+
+    app_timer_start(m_qre1113gr_timer, APP_TIMER_TICKS(QRE1113GR_WAIT_INTERVAL_MS), NULL);
+}
+
+static void on_f_measured(uint16_t value)
+{
+    NRF_LOG_INFO("on_f_measured : %d", value);
+
+    m_temp_data.f = value < QRE1113GR_DETECT_DISTANCE;
+
+    nrf_gpio_pin_write(QRE1113GR_PIN_F_K, 1);
+
+    app_timer_start(m_qre1113gr_timer, APP_TIMER_TICKS(QRE1113GR_WAIT_INTERVAL_MS), NULL);
+}
+
+static void on_g_measured(uint16_t value)
+{
+    NRF_LOG_INFO("on_g_measured : %d", value);
+
+    m_temp_data.g = value < QRE1113GR_DETECT_DISTANCE;
+
+    nrf_gpio_pin_write(QRE1113GR_PIN_G_K, 1);
+
+    app_timer_start(m_qre1113gr_timer, APP_TIMER_TICKS(QRE1113GR_WAIT_INTERVAL_MS), NULL);
+}
+
+static void on_h_measured(uint16_t value)
+{
+    NRF_LOG_INFO("on_h_measured : %d", value);
+
+    m_temp_data.h = value < QRE1113GR_DETECT_DISTANCE;
+
+    nrf_gpio_pin_write(QRE1113GR_PIN_H_K, 1);
+    multiplexer_set_enabled(false);
 
     memcpy(&m_data, &m_temp_data, sizeof(qre1113gr_data));
 
@@ -74,46 +124,110 @@ static void qre1113gr_timeout(void *p_context)
     {
     case 0:
     {
-        err_code = n_saadc_measure(QRE1113GR_PIN_A_COL, on_a_measured);
+        err_code = n_saadc_measure(QRE1113GR_PIN_COL, on_a_measured);
         APP_ERROR_CHECK(err_code);
         break;
     }
     case 1:
     {
         nrf_gpio_pin_write(QRE1113GR_PIN_B_K, 0);
+        multiplexer_set_bit(1);
 
         app_timer_start(m_qre1113gr_timer, APP_TIMER_TICKS(QRE1113GR_READ_INTERVAL_MS), NULL);
         break;
     }
     case 2:
     {
-        err_code = n_saadc_measure(QRE1113GR_PIN_B_COL, on_b_measured);
+        err_code = n_saadc_measure(QRE1113GR_PIN_COL, on_b_measured);
         APP_ERROR_CHECK(err_code);
         break;
     }
     case 3:
     {
         nrf_gpio_pin_write(QRE1113GR_PIN_C_K, 0);
+        multiplexer_set_bit(2);
 
         app_timer_start(m_qre1113gr_timer, APP_TIMER_TICKS(QRE1113GR_READ_INTERVAL_MS), NULL);
         break;
     }
     case 4:
     {
-        err_code = n_saadc_measure(QRE1113GR_PIN_C_COL, on_c_measured);
+        err_code = n_saadc_measure(QRE1113GR_PIN_COL, on_c_measured);
         APP_ERROR_CHECK(err_code);
         break;
     }
     case 5:
     {
         nrf_gpio_pin_write(QRE1113GR_PIN_D_K, 0);
+        multiplexer_set_bit(3);
 
         app_timer_start(m_qre1113gr_timer, APP_TIMER_TICKS(QRE1113GR_READ_INTERVAL_MS), NULL);
         break;
     }
+
     case 6:
     {
-        err_code = n_saadc_measure(QRE1113GR_PIN_D_COL, on_d_measured);
+        err_code = n_saadc_measure(QRE1113GR_PIN_COL, on_d_measured);
+        APP_ERROR_CHECK(err_code);
+        break;
+    }
+    case 7:
+    {
+        nrf_gpio_pin_write(QRE1113GR_PIN_E_K, 0);
+        multiplexer_set_bit(3);
+
+        app_timer_start(m_qre1113gr_timer, APP_TIMER_TICKS(QRE1113GR_READ_INTERVAL_MS), NULL);
+        break;
+    }
+
+    case 8:
+    {
+        err_code = n_saadc_measure(QRE1113GR_PIN_COL, on_e_measured);
+        APP_ERROR_CHECK(err_code);
+        break;
+    }
+    case 9:
+    {
+        nrf_gpio_pin_write(QRE1113GR_PIN_F_K, 0);
+        multiplexer_set_bit(3);
+
+        app_timer_start(m_qre1113gr_timer, APP_TIMER_TICKS(QRE1113GR_READ_INTERVAL_MS), NULL);
+        break;
+    }
+
+    case 10:
+    {
+        err_code = n_saadc_measure(QRE1113GR_PIN_COL, on_f_measured);
+        APP_ERROR_CHECK(err_code);
+        break;
+    }
+    case 11:
+    {
+        nrf_gpio_pin_write(QRE1113GR_PIN_G_K, 0);
+        multiplexer_set_bit(3);
+
+        app_timer_start(m_qre1113gr_timer, APP_TIMER_TICKS(QRE1113GR_READ_INTERVAL_MS), NULL);
+        break;
+    }
+
+    case 12:
+    {
+        err_code = n_saadc_measure(QRE1113GR_PIN_COL, on_g_measured);
+        APP_ERROR_CHECK(err_code);
+        break;
+    }
+    case 13:
+    {
+        nrf_gpio_pin_write(QRE1113GR_PIN_H_K, 0);
+        multiplexer_set_bit(3);
+
+        app_timer_start(m_qre1113gr_timer, APP_TIMER_TICKS(QRE1113GR_READ_INTERVAL_MS), NULL);
+        break;
+    }
+
+    case 14:
+    {
+        err_code = n_saadc_measure(QRE1113GR_PIN_COL, on_h_measured);
         APP_ERROR_CHECK(err_code);
 
         break;
@@ -131,15 +245,23 @@ ret_code_t qre1113gr_init(qre1113gr_cb_t on_ir_measured)
     nrf_gpio_cfg_output(QRE1113GR_PIN_B_K);
     nrf_gpio_cfg_output(QRE1113GR_PIN_C_K);
     nrf_gpio_cfg_output(QRE1113GR_PIN_D_K);
+    nrf_gpio_cfg_output(QRE1113GR_PIN_E_K);
+    nrf_gpio_cfg_output(QRE1113GR_PIN_F_K);
+    nrf_gpio_cfg_output(QRE1113GR_PIN_G_K);
+    nrf_gpio_cfg_output(QRE1113GR_PIN_H_K);
 
     nrf_gpio_pin_write(QRE1113GR_PIN_A_K, 1);
     nrf_gpio_pin_write(QRE1113GR_PIN_B_K, 1);
     nrf_gpio_pin_write(QRE1113GR_PIN_C_K, 1);
     nrf_gpio_pin_write(QRE1113GR_PIN_D_K, 1);
+    nrf_gpio_pin_write(QRE1113GR_PIN_E_K, 1);
+    nrf_gpio_pin_write(QRE1113GR_PIN_F_K, 1);
+    nrf_gpio_pin_write(QRE1113GR_PIN_G_K, 1);
+    nrf_gpio_pin_write(QRE1113GR_PIN_H_K, 1);
 
     err_code = app_timer_create(&m_qre1113gr_timer,
-        APP_TIMER_MODE_SINGLE_SHOT,
-        qre1113gr_timeout);
+                                APP_TIMER_MODE_SINGLE_SHOT,
+                                qre1113gr_timeout);
 
     m_on_ir_measured = on_ir_measured;
 
@@ -161,6 +283,8 @@ ret_code_t qre1113gr_update()
     m_idx_reading = 0;
 
     m_reading = true;
+    multiplexer_set_enabled(true);
+    multiplexer_set_bit(0);
 
     nrf_gpio_pin_write(QRE1113GR_PIN_A_K, 0);
 
--- neosarchizo/components/drivers/qre1113gr/qre1113gr.h
+++ neosarchizo/components/drivers/qre1113gr/qre1113gr.h
@@ -10,15 +10,16 @@ extern "C"
 {
 #endif
 
-#define QRE1113GR_PIN_A_COL NRF_SAADC_INPUT_AIN0
-#define QRE1113GR_PIN_B_COL NRF_SAADC_INPUT_AIN1
-#define QRE1113GR_PIN_C_COL NRF_SAADC_INPUT_AIN2
-#define QRE1113GR_PIN_D_COL NRF_SAADC_INPUT_AIN3
+#define QRE1113GR_PIN_COL NRF_SAADC_INPUT_AIN0
 
-#define QRE1113GR_PIN_A_K 9
-#define QRE1113GR_PIN_B_K 16
-#define QRE1113GR_PIN_C_K 14
+#define QRE1113GR_PIN_A_K 8
+#define QRE1113GR_PIN_B_K 9
+#define QRE1113GR_PIN_C_K 10
 #define QRE1113GR_PIN_D_K 11
+#define QRE1113GR_PIN_E_K 12
+#define QRE1113GR_PIN_F_K 14
+#define QRE1113GR_PIN_G_K 15
+#define QRE1113GR_PIN_H_K 16
 
 // #define QRE1113GR_DETECT_DISTANCE 14100
 #define QRE1113GR_DETECT_DISTANCE 13600
@@ -32,6 +33,10 @@ extern "C"
         uint8_t b;
         uint8_t c;
         uint8_t d;
+        uint8_t e;
+        uint8_t f;
+        uint8_t g;
+        uint8_t h;
     } qre1113gr_data;
 
     typedef void (*qre1113gr_cb_t)(qre1113gr_data data);
