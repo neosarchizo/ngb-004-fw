44ab61aa2f4f4fa001bbc97bf7397dd2fb39fb37
neosarchizo <neosarchizo@gmail.com>
2021-03-26 00:01:20 +0900

    feat: update scripts

--- projects/generate_settings.sh
+++ projects/generate_settings.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "generate settings"
-nrfutil settings generate --family NRF52810 --application ./switch/Output/Release/Exe/switch.hex --application-version 1 --bootloader-version 0 --bl-settings-version 2 bootloader_setting.hex
+nrfutil settings generate --family NRF52810 --application ./spb2-p8/Output/Release/Exe/spb2_p8.hex --application-version 0 --bootloader-version 0 --bl-settings-version 2 bootloader_setting.hex
 echo "done!"
 
--- projects/merge_all_hex.sh
+++ projects/merge_all_hex.sh
@@ -11,11 +11,11 @@ if test -f "app-settings-bl-sd.hex"; then
 fi
 
 echo "1) application + settings"
-mergehex -m ./switch/Output/Release/Exe/switch.hex bootloader_setting.hex -o app-settings.hex
+mergehex -m ./spb2-p8/Output/Release/Exe/spb2_p8.hex bootloader_setting.hex -o app-settings.hex
 echo "2) 1) + bootloader"
 mergehex -m app-settings.hex ./bootloader/Output/Release/Exe/bootloader.hex -o app-settings-bl.hex
 echo "3) 2) + SD"
-mergehex -m app-settings-bl.hex ../nordic/nRF5_SDK_16.0.0_98a08e2/components/softdevice/s112/hex/s112_nrf52_7.0.1_softdevice.hex -o app-settings-bl-sd.hex
+mergehex -m app-settings-bl.hex ../nordic/nRF5_SDK_15.3.0_59ac345/components/softdevice/s112/hex/s112_nrf52_6.1.1_softdevice.hex -o app-settings-bl-sd.hex
 
 echo "done!"
 
