75ce1bd36b4e25f96e38f4bdbdb43e00ea542268
neosarchizo <neosarchizo@gmail.com>
2021-04-09 15:45:48 +0900

    feat: update generate_zip.sh

--- projects/generate_zip.sh
+++ projects/generate_zip.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "generate application zip"
-nrfutil pkg generate --hw-version 52 --application-version $1 --application ./spb2-p8/Output/Release/Exe/spb2_p8.hex --sd-req 0xCD --key-file ./bootloader/private.key "app_$1.zip"
+nrfutil pkg generate --hw-version 52 --application-version $1 --application ./spb2-p8/Output/Release/Exe/spb2_p8.hex --sd-req 0xB8 --key-file ./bootloader/private.key "app_$1.zip"
 echo "done!"
